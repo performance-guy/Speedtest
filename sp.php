a5947dcfd1ae01126090e801a5be19bae322b7a9
salmansaleem920 <salmansaleem920@hotmail.com>
2019-09-15 14:02:45 +0500

    db

--- sp.php
+++ sp.php
@@ -6,7 +6,8 @@ use Spatie\Async\Pool;
 use Entrecore\GTMetrixClient\GTMetrixClient;
 use Entrecore\GTMetrixClient\GTMetrixTest;
 
-
+$web  = "";
+$email = "";
 // Check if the form is submitted
 if ( isset( $_POST['submit'] ) ) {
 
@@ -45,7 +46,7 @@ $email = $_GET['email'];
 
 // display the results
 echo 'Your data is ' . $web .' ' . $email;
-exit;
+
 }
 
 
@@ -56,99 +57,109 @@ $client->setAPIKey('95d9d685cacc92669804814e18712e00');
 
 $client->getLocations();
 $client->getBrowsers();
-$test = $client->startTest('https://www.kinsta.com/');
+$test = $client->startTest($web);
 
-//Wait for result
-while ($test->getState() != GTMetrixTest::STATE_COMPLETED &&
-    $test->getState() != GTMetrixTest::STATE_ERROR) {
-    $client->getTestStatus($test);
-    sleep(5);
+////Wait for result
+//while ($test->getState() != GTMetrixTest::STATE_COMPLETED &&
+//    $test->getState() != GTMetrixTest::STATE_ERROR) {
+//    $client->getTestStatus($test);
+//    sleep(5);
+//}
+echo 'salman';
+echo 'salman';
+
+
+
+//if ($done)
+//{
+//    header("Location: /url/to/the/other/page");
+//    exit;
+//}
+
+
+
+
+
+
+
+
+$wpt = new WebPageTest('A.fe5ef47b69dfa003467133609b7a701d');
+$test_id ='';
+
+if ($response = $wpt->runTest('https://www.kinsta.com')) {
+    if ($response->statusCode == StatusCode::OK) {
+        // All test info is available in $response->data.
+        $test_id = $response->data->testId;
+    }
 }
-echo 'salman';
-echo 'salman';
+ $response = $wpt->getTestStatus($test_id);
+        // All test info is available in $response->data.
+        while ($response->statusCode != StatusCode::OK) {
+            $response = $wpt->getTestStatus($test_id);
+            if ($response->statusCode == StatusCode::OK) {
+                echo var_dump($response);
+                break;
+            } else if ($response->statusCode == StatusCode::CONTINUING) {
+
+                $pool = Pool::create();
+
+                foreach (range(1, 20) as $i) {
+                    $pool[] = async(function () {
+                        usleep(random_int(10, 1000));
+
+                        return 2;
+                    })->then(function (int $output) {
+                        echo $output;
+                    });
+                }
+
+                await($pool);
+
+            } else if ($response->startCode == StatusCode::SWITCHING_PROTOCOLS || $response->startCode == 101 || true) {
+                $pool = Pool::create();
+
+                foreach (range(1, 20) as $i) {
+                    $pool[] = async(function () {
+                        usleep(random_int(10, 1000));
+
+                        return 2;
+                    })->then(function (int $output) {
+                        echo $output;
+                    });
+                }
+
+                await($pool);
+
+            } else if ($response->statusCode == StatusCode::PAYMENT_REQUIRED) {
+                // Test has been cancelled.
+            }
+        }
 
 
 
+$full_response = "";
+if ($response = $wpt->getTestResults($test_id)) {
+    // All test result info is available in $response->data.
+    if ($response->statusCode == StatusCode::OK) {
 
+        // Test is complete.
+        $full_response = $response;
+        echo var_dump($response);
 
+    }
+    else if (in_array($response->statusCode, [StatusCode::CONTINUING, StatusCode::SWITCHING_PROTOCOLS])) {
+        // Test is not yet complete.
+    }
+    else {
+        // Test failed.
+    }
+}
 
+if ($response = $wpt->getLocations()) {
+    if ($response->statusCode == StatusCode::OK) {
+        // All locations info is available in $response->data.
 
+        echo var_dump($response);
 
-
-
-
-
-//$wpt = new WebPageTest('A.fe5ef47b69dfa003467133609b7a701d');
-//$test_id ='';
-//
-//if ($response = $wpt->runTest('https://www.kinsta.com')) {
-//    if ($response->statusCode == StatusCode::OK) {
-//        // All test info is available in $response->data.
-//        $test_id = $response->data->testId;
-//    }
-//}
-// $response = $wpt->getTestStatus($test_id);
-//        // All test info is available in $response->data.
-//        while ($response->statusCode != StatusCode::OK) {
-//            $response = $wpt->getTestStatus($test_id);
-//            if ($response->statusCode == StatusCode::OK) {
-//                echo var_dump($response);
-//                break;
-//            } else if ($response->statusCode == StatusCode::CONTINUING) {
-//
-//                $pool = Pool::create();
-//
-//                foreach (range(1, 20) as $i) {
-//                    $pool[] = async(function () {
-//                        usleep(random_int(10, 1000));
-//
-//                        return 2;
-//                    })->then(function (int $output) {
-//                        echo $output;
-//                    });
-//                }
-//
-//                await($pool);
-//
-//            } else if ($response->startCode == StatusCode::SWITCHING_PROTOCOLS || $response->startCode == 101 || true) {
-//                $pool = Pool::create();
-//
-//                foreach (range(1, 20) as $i) {
-//                    $pool[] = async(function () {
-//                        usleep(random_int(10, 1000));
-//
-//                        return 2;
-//                    })->then(function (int $output) {
-//                        echo $output;
-//                    });
-//                }
-//
-//                await($pool);
-//
-//            } else if ($response->statusCode == StatusCode::PAYMENT_REQUIRED) {
-//                // Test has been cancelled.
-//            }
-//        }
-//
-//
-//
-//
-//if ($response = $wpt->getTestResults($test_id)) {
-//    // All test result info is available in $response->data.
-//    if ($response->statusCode == StatusCode::OK) {
-//        // Test is complete.
-//
-//    }
-//    else if (in_array($response->statusCode, [StatusCode::CONTINUING, StatusCode::SWITCHING_PROTOCOLS])) {
-//        // Test is not yet complete.
-//    }
-//    else {
-//        // Test failed.
-//    }
-//}
-//
-//if ($response = $wpt->getLocations()) {
-//    if ($response->statusCode == StatusCode::OK) {
-//        // All locations info is available in $response->data.
-//    }
-//}
+    }
+}
