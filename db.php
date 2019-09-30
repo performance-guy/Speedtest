8009fd2e04ec22de139221c59bba1320d1a6ed3e
Salman <salmansaleem920@hotmail.com>
2019-09-30 23:11:18 +0500

    Done 30 October 2019

--- db.php
+++ db.php
@@ -18,11 +18,20 @@ function getWebsiteData($id)
 {
     $con = OpenCon();
     $sql = mysqli_query($con, "SELECT * FROM website_test where ID = '$id'");
-
+    $data[] = "";
     while ($row = mysqli_fetch_array($sql)) {
-        $names[] = $row[2];
-        $a = $row['ID'];
+        $data["f_cache"] = $row['F_Cache'];
+        $data["f_load_time"] = $row['F_Load_Time'];
+        $data["f_render_time"] = $row['F_Render_Time'];
+        $data["f_page_size"] = $row['F_Page_Size'];
+        $data["f_request"] = $row['F_Request'];
+        $data["r_cache"] = $row['R_Cache'];
+        $data["r_load_time"] = $row['R_Load_Time'];
+        $data["r_render_time"] = $row['R_Render_Time'];
+        $data["r_page_size"] = $row['R_Page_Size'];
+        $data["r_request"] = $row['R_Request'];
     }
-    $a =$names;
+
+    return $data;
     //asf
 }
--- result.php
+++ result.php
@@ -18,7 +18,7 @@
         include 'db.php';
         $id =  $_GET['id'];
         $website =  $_GET['website'];
-        getWebsiteData($id);
+        $rrr = getWebsiteData($id);
 
 ?>
 
@@ -26,30 +26,58 @@
     <div class="row w-100">
         <div class="col-md-3">
             <div class="card border-info mx-sm-1 p-3">
-
-                <div class="text-info text-center mt-3"><h4>Cars</h4></div>
-                <div class="text-info text-center mt-2"><h1>234</h1></div>
+                <div class="text-info text-center mt-3"><h4>First View loadtime</h4></div>
+                <div class="text-info text-center mt-2"><h1><?php echo $rrr["f_load_time"]?></h1></div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card border-success mx-sm-1 p-3">
-                <div class="text-success text-center mt-3"><h4>Eyes</h4></div>
-                <div class="text-success text-center mt-2"><h1>9332</h1></div>
+                <div class="text-success text-center mt-3"><h4>First View Render Time</h4></div>
+                <div class="text-success text-center mt-2"><h1><?php echo $rrr["f_render_time"]?></h1></div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card border-danger mx-sm-1 p-3">
-                <div class="text-danger text-center mt-3"><h4>Hearts</h4></div>
-                <div class="text-danger text-center mt-2"><h1>346</h1></div>
+                <div class="text-danger text-center mt-3"><h4>First View Page Size</h4></div>
+                <div class="text-danger text-center mt-2"><h1><?php echo $rrr["f_page_size"]?></h1></div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card border-warning mx-sm-1 p-3">
-                <div class="text-warning text-center mt-3"><h4>Inbox</h4></div>
-                <div class="text-warning text-center mt-2"><h1>346</h1></div>
+                <div class="text-warning text-center mt-3"><h4>First View Request</h4></div>
+                <div class="text-warning text-center mt-2"><h1><?php echo $rrr["f_request"]?></h1></div>
             </div>
         </div>
     </div>
 </div>
+    <div class="jumbotron">
+        <div class="row w-100">
+            <div class="col-md-3">
+                <div class="card border-info mx-sm-1 p-3">
+                    <div class="text-info text-center mt-3"><h4>Repeat View loadtime</h4></div>
+                    <div class="text-info text-center mt-2"><h1><?php echo $rrr["r_load_time"]?></h1></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-success mx-sm-1 p-3">
+                    <div class="text-success text-center mt-3"><h4>Repeat View Render Time</h4></div>
+                    <div class="text-success text-center mt-2"><h1><?php echo $rrr["r_render_time"]?></h1></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-danger mx-sm-1 p-3">
+                    <div class="text-danger text-center mt-3"><h4>Repeat View Page Size</h4></div>
+                    <div class="text-danger text-center mt-2"><h1><?php echo $rrr["r_page_size"]?></h1></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border-warning mx-sm-1 p-3">
+                    <div class="text-warning text-center mt-3"><h4>Repeat View Request</h4></div>
+                    <div class="text-warning text-center mt-2"><h1><?php echo $rrr["r_request"]?></h1></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
